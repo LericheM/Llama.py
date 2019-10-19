fb5c14a3c5d72d5d1beecdc41862d45470642704
LericheM <ml529@njit.edu>
2019-10-19 17:25:33 -0400

    testing responding to users

--- bot.py
+++ bot.py
@@ -2,14 +2,31 @@ import os
 import discord
 import bot_log
 import bot_creds
+from dotenv import load_dotenv
+load_dotenv()
+
 
 TOKEN = bot_creds.token
+
 client = discord.Client()
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
+    
     bot_channel = message.channel
     print(bot_channel)
-    
+    to_user = message.author
+    print(to_user)
+    if(message.channel == bot_channel):
+        at_user = message.author.mention
+        await bot_channel.send(
+            f"Hello @{at_user} ."
+        )
+
+@client.event
+async def on_ready():
+    print(f"{client.user} has connected to discord!")
+
+client.run(TOKEN)
